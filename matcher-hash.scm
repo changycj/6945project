0d39688153eb45a747526dfd95497e780818dd9f
Yachi Chang <changycj@mit.edu>
2015-05-04 20:52:30 -0400

    new hashing function

--- matcher-hash.scm
+++ matcher-hash.scm
@@ -2,32 +2,56 @@
 
 (define make-chord-difference-table make-equal-hash-table)
 
-(define (chord-difference->int table chord-difference)
-  (let ((val (hash-table/get table chord-difference #f)))
+(define (chord-difference->int table chord-difference scheme)
+  (let ((val (get-chord-difference->int table chord-difference scheme)))
     (if val
 	val
-	(add-chord-difference->int table chord-difference))))
+	(add-chord-difference->int table chord-difference scheme))))
 
-(define (add-chord-difference->int table chord-difference)
+(define (add-chord-difference->int table chord-difference scheme)
+  (cond ((eq? scheme 'exact-matching)
+	 (exact-matching table chord-difference))
+	((eq? scheme 'exact-pitch-matching)
+	 (exact-pitch-matching table chord-difference))))
+
+(define (get-chord-difference->int table chord-difference scheme)
+  (cond ((eq? scheme 'exact-matching)
+	 (hash-table/get table chord-difference #f))
+	((eq? scheme 'exact-pitch-matching)
+	 (let ((delta (chord-difference-pitch-delta chord-difference)))
+	   (if (> (abs delta) 500)
+	       (hash-table/get table 1000 #f)
+	       (hash-table/get table delta #f))))))
+
+;; Exact Matching
+(define (exact-matching table chord-difference)
   (let ((l (hash-table/count table)))
     (hash-table/put! table chord-difference l)
     l))
 
-(define (chord-differences->ints chord-differences)
+;; Exact Pitch Matching (different hash for different pitch delta only)
+(define (exact-pitch-matching table chord-difference)
+  (let ((l (hash-table/count table))
+	(delta (chord-difference-pitch-delta chord-difference)))
+    (if (> (abs delta) 500)
+	(hash-table/put! table 1000 l)
+	(hash-table/put! table delta l))
+    l))
+
+(define (chord-differences->ints chord-differences scheme)
   (let ((vec (make-vector (length chord-differences)))
 	(table (make-chord-difference-table)))
     (let iter ((ind 0))
       (if (= ind (length chord-differences))
 	  (vector->list vec)
 	  (let ((chord-difference (list-ref chord-differences ind)))
-	    (let ((val (chord-difference->int table chord-difference)))
-	      (vector-set! vec
-			   ind
-			   (if val
-			       val
-			       (add-chord-difference->int table chord-difference)))
+	    (let ((val (chord-difference->int table chord-difference scheme)))
+	      (vector-set! vec ind val)
 	      (iter (+ ind 1))))))))
 
 (define (difference-profile->ints difference-profile)
-  (chord-differences->ints (difference-profile-chord-differences difference-profile)))
+  (chord-differences->ints
+   (difference-profile-chord-differences difference-profile)
+   ;; SPECIFY MATCHING SCHEME HERE
+   'exact-pitch-matching))
 				     
